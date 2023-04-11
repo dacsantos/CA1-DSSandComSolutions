d084f39a3eeb47438fb4dde2ef9a294fb47aa09a
dacsantos <72028806+dacsantos@users.noreply.github.com>
2023-04-11 16:16:20 +0100

    Spring Initializer
    - Created Springboot project with dependencies

---
+++ src/main/java/com/cctdkl/dssandcomsolutions/mainapp/MainApp/MainApp.java
@@ -0,0 +1,13 @@
+package com.cctdkl.dssandcomsolutions.mainapp.MainApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/cctdkl/dssandcomsolutions/mainapp/MainApp/MainAppTests.java
@@ -0,0 +1,13 @@
+package com.cctdkl.dssandcomsolutions.mainapp.MainApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
